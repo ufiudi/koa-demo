33613bfae0ee6bf4f63a8739a182b46e528eb849
gaochengyao <461045246@qq.com>
2022-01-26 16:37:41 +0800

    数据库

--- db/index.js
+++ db/index.js
@@ -1,7 +1,8 @@
-const user = 'gaochengyao'
-const password = 'gao7641081'
+const user = 'test'
+const password = 'test123'
 const db = 'test'
-const url = `mongodb+srv://${user}:${password}@gcylearn.jyjni.mongodb.net/${db}`
+// const url = `mongodb+srv://${user}:${password}@106.12.200.48/${db}`
+const url = `mongodb://${user}:${password}@106.12.200.48:27017/?authSource=${db}&readPreference=primary&appname=MongoDB%20Compass&ssl=false`
 const mongoose = require('mongoose')
 mongoose.connect(url)
 mongoose.connection.once('open', () => {
